5a1556e86e89163c7dbabbbc0b795f7cd635306d
ialikh72 <ialikh.w1@gmail.com>
2026-05-11 23:14:42 +0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator_GUI
+{
+    public partial class Form1 : Form
+    {
+        string currentInput = "";
+        double result = 0;
+        string operation = "";
+        bool operationPending = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            currentInput = textBox1.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentInput += button.Text;
+            textBox1.Text = currentInput;
+        }
+
+        private void Evaluate()
+        {
+            if (operationPending)
+            {
+                double secondNumber = double.Parse(currentInput);
+
+                switch (operation)
+                {
+                    case "+":
+                        result += secondNumber;
+                        break;
+                    case "-":
+                        result -= secondNumber;
+                        break;
+                    case "*":
+                        result *= secondNumber;
+                        break;
+                    case "/":
+                        if (secondNumber != 0)
+                        {
+                            result /= secondNumber;
+                        }
+                        else
+                        {
+                            textBox1.Text = "Error";
+                            return;
+                        }
+                        break;
+                }
+                textBox1.Text = result.ToString();
+                currentInput = "";
+                operationPending = false;
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (operationPending)
+            {
+                Evaluate();
+            }
+
+            Button button = (Button)sender;
+            operation = button.Text;
+            result = double.Parse(currentInput);
+            currentInput = "";
+            operationPending = true;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (operationPending)
+            {
+                Evaluate();
+            }
+
+            Button button = (Button)sender;
+            operation = button.Text;
+            result = double.Parse(currentInput);
+            currentInput = "";
+            operationPending = true;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (operationPending)
+            {
+                Evaluate();
+            }
+
+            Button button = (Button)sender;
+            operation = button.Text;
+            result = double.Parse(currentInput);
+            currentInput = "";
+            operationPending = true;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (operationPending)
+            {
+                Evaluate();
+            }
+
+            Button button = (Button)sender;
+            operation = button.Text;
+            result = double.Parse(currentInput);
+            currentInput = "";
+            operationPending = true;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            currentInput = "";
+            result = 0;
+            operation = "";
+            operationPending = false;
+            textBox1.Text = "";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Evaluate();
+        }
+    }
+}
